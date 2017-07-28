9d0a0518f514f08674bb30f593d827a3883e08f4
Chris Drain <DrainC4@yahoo.com>
2017-07-28 14:13:36 -0700

    update info working now must display data and require password

--- inc.phpLogic/updateInfo.php
+++ inc.phpLogic/updateInfo.php
@@ -3,67 +3,83 @@
 session_start();
 //include connection
 include("grievance.php");
+$employeeID = $_POST['eid'];
+$employeeType = $_POST['employeeStatus'];
+$address = $_POST['address'];
+$city = $_POST['city'];
+$state = $_POST['state'];
+$zipCode = $_POST['zipCode'];
+$phone = $_POST['phone'];
+$seniority = $_POST['seniority'];
+$payStatus = $_POST['payLevel'];
+$payStep = $_POST['payStep'];
+$tour = $_POST['tour'];
+$daysOff = $_POST['daysOff'];
+$veteran = $_POST['veteranStatus'];
+$layOffProtected = $_POST['layOffProtected'];
+$email = $_SESSION['email'];
 //on submit transaction will begin
 if($_SERVER['REQUEST_METHOD']=='POST'){
   try{
   $conn->beginTransaction();
-if(isset($_POST['eid'])) {
-$f_n = $conn->prepare("update UserSignUp Set employeeID = ".$_POST['eid']. " where emailAddress = ".$_SESSION['email']);
+if(!empty($_POST['eid'])) {
+$f_n = $conn->prepare("update UserSignUp Set employeeID = '$employeeID'  where emailAddress = '$email'");
 $f_n->execute();
 }
-if(isset($_POST['employeeStatus'])) {
-$e_s = $conn->prepare("update UserSignUp Set employeeType = " .$_POST['employeeStatus']. " where emailAddress = " .$_SESSION['email']);
+if(!empty($_POST['employeeStatus'])) {
+$e_s = $conn->prepare("update UserSignUp Set employeeType = '$employeeType' where emailAddress = '$email'");
 $e_s->execute();
 }
-if(isset($_POST['address'])) {
-$a_u = $conn->prepare("update UserSignUp Set address = " .$_POST['address']. " where emailAddress = " .$_SESSION['email']);
+if(!empty($_POST['address'])) {
+$a_u = $conn->prepare("update UserSignUp Set address = '$address' where emailAddress = '$email'");
 $a_u->execute();
 }
-if(isset($_POST['city'])) {
-$c_u = $conn->prepare("update UserSignUp Set city = ".$_POST['city']." where emailAddress = " .$_SESSION['email']);
+if(!empty($_POST['city'])) {
+$c_u = $conn->prepare("update UserSignUp Set city = '$city' where emailAddress = '$email'");
 $c_u->execute();
 }
-if(isset($_POST['state'])) {
-$s_u = $conn->prepare("update UserSignUp Set state =" .$_POST['state']. " where emailAddress = ".$_SESSION['email']);
+if(!empty($_POST['state'])) {
+$s_u = $conn->prepare("update UserSignUp Set state = '$state' where emailAddress = '$email'");
 $s_u->execute();
 }
-if(isset($_POST['zipCode'])) {
-$z_u = $conn->prepare("update UserSignUp Set zipCode = " .$_POST['zipCode']. " where emailAddress = ".$_SESSION['email']);
+if(!empty($zipCode)) {
+$z_u = $conn->prepare("update UserSignUp Set zipCode = '$zipCode' where emailAddress = '$email'");
 $z_u->execute();
 }
-if(isset($_POST['phone'])) {
-$p_n = $conn->prepare("update UserSignUp Set phoneNumber = " .$_POST['phone']. "where emailAddress = ".$_SESSION['email']);
+if(!empty($_POST['phone'])) {
+$p_n = $conn->prepare("update UserSignUp Set phoneNumber = '$phone' where emailAddress = '$email'");
 $p_n->execute();
 }
-if(isset($_POST['seniority'])) {
-$s_d = $conn->prepare("update UserSignUp Set seniorityDate = " .$_POST['seniority']. " where emailAddress = ".$_SESSION['email']);
+if(!empty($_POST['seniority'])) {
+$s_d = $conn->prepare("update UserSignUp Set seniorityDate = '$seniority' where emailAddress = '$email'");
 $s_d->execute();
 }
-if(isset($_POST['payLevel'])) {
-$p_l = $conn->prepare("update UserSignUp Set payLevel = " .$_POST['payLevel']. "where emailAddress = " .$_SESSION['email']);
+if(!empty($_POST['payLevel'])) {
+$p_l = $conn->prepare("update UserSignUp Set payLevel = '$payStatus' where emailAddress = '$email'");
 $p_l->execute();
 }
-if(isset($_POST['payStep'])) {
-$p_s = $conn->prepare("update UserSignUp Set payStep = " .$_POST['payStep']. "where emailAddress = " .$_SESSION['email']);
+if(!empty($_POST['payStep'])) {
+$p_s = $conn->prepare("update UserSignUp Set payStep = '$payStep' where emailAddress = '$email'");
 $p_s->execute();
 }
-if(isset($_POST['tour'])) {
-$t_u = $conn->prepare("update UserSignUp Set tour = " .$_POST['tour']. "where emailAddress = " .$_SESSION['email']);
+if(!empty($_POST['tour'])) {
+$t_u = $conn->prepare("update UserSignUp Set tour = '$tour' where emailAddress = '$email'");
 $t_u->execute();
 }
-if(isset($_POST['daysOff'])) {
-$d_o = $conn->prepare("update UserSignUp Set daysOff = " .$_POST['daysOff']. "where emailAddress = ".$_SESSION['email']);
+if(!empty($_POST['daysOff'])) {
+$d_o = $conn->prepare("update UserSignUp Set daysOff = '$daysOff' where emailAddress = '$email'");
 $d_o->execute();
 }
-if(isset($_POST['veteranStatus'])) {
-$v_n = $conn->prepare("update UserSignUp Set veteranStatus = " .$_POST['veteranStatus']. "where emailAddress = " .$_SESSION['email']);
+if(!empty($_POST['veteranStatus'])) {
+$v_n = $conn->prepare("update UserSignUp Set veteranStatus = '$veteran' where emailAddress = '$email'");
 $v_n->execute();
 }
-if(isset($_POST['layOffProtected'])) {
-$l_p = $conn->prepare("update UserSignUp Set layOffProtected = " .$_POST['layOffProtected']. "where emailAddress = " .$_SESSION['email']);
+if(!empty($_POST['layOffProtected'])) {
+$l_p = $conn->prepare("update UserSignUp Set layOffProtected = '$layOffProtected' where emailAddress = '$email'");
 $l_p->execute();
 }
 $conn->commit();
+header('location:../newUpdateAccountInfo.html');
 }
 
 catch(PDOException $e) {
