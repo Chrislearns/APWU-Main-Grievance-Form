8ec1bc888fe9bc2ad17a2ee50da595c97e843235
Chris Drain <DrainC4@yahoo.com>
2017-07-29 18:07:48 -0700

    fixed before work!!

--- inc.phpLogic/validatelogin.php
+++ inc.phpLogic/validatelogin.php
@@ -17,9 +17,9 @@ include("grievance.php");
 //Set POST variables
 $email = htmlentities(trim($_POST['user_email']),ENT_QUOTES, "UTF-8");
 $password = htmlentities(trim($_POST['password']),ENT_QUOTES, "UTF-8");
-if($_SERVER['REQUEST_METHOD' == 'POST']){
+
 //Start of query
-//verify hash from database
+
 $query = "select * from userAccounts where emailAddress = :email";
 $stmt = $conn->prepare($query);
 $stmt->bindParam(':email', $email);
@@ -28,10 +28,12 @@ $count = $stmt->rowCount();
 $results = $stmt->fetch(PDO::FETCH_ASSOC);
 $dbpassword = $results['PASSWORD'];
 $_SESSION['name'] = $results['fullName'];
+$_SESSION['email']= $email;
 $verify = password_verify($password, $dbpassword);
 
 
 //verify if their is one row
+//verify hash from database
 if($count == 1 && $verify){
   //Get employee ID for sessions
   $queryid = "select * from UserSignUp where emailAddress = :email ";
@@ -53,20 +55,13 @@ if($count == 1 && $verify){
 
             }
       }
-}
-else{
-  session_unset();
-  session_destroy();
-  session_write_close();
-  setcookie(session_name(),'',0,'/');
-  session_regenerate_id(true);
-  if (session_status() == PHP_SESSION_NONE){
-  session_start();
+      else{
 
-   $_SESSION["error"] = "<h4>Invalid Log-in creditials</h4>";
 
-  header('location:../newLogInPage.php');
-}
-  $conn = null;
-    exit;
-}
+         $_SESSION["error"] = "<h4>Invalid Log-in creditials</h4>";
+
+        header('location:../newLogInPage.php');
+
+        $conn = null;
+          exit;
+      }
